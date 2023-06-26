ba2006d79fe8c07a0cd1bef87f5341ef0750cc05
朱重迁 <3266420686@qq.com>
2023-06-26 13:52:33 +0800

    测试1111

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    111111111111111112222
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
